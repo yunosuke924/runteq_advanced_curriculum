8aed3cc54930ea847918efa4ad63a1d71e43887a
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-16 05:44:41 +0900

    finish

---
+++ spec/system/mediums_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "Mediums", type: :system do
+  describe 'コンテンツブロックを挿入' do
+    context '画像ファイルをアップロードしない' do
+      it 'プレビューが表示される'
+      it '公開ページが表示される'
+    end
+  end
+end
